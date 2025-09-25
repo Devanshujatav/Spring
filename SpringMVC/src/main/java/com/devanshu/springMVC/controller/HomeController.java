adcc3e1b5da5f81ea489a90d7cb6763419a625a7
Devanshu Jatav <devanshujatav2003@gmail.com>
2025-09-25 07:04:50 +0530

    SpringMVC

--- SpringMVC/src/main/java/com/devanshu/springMVC/controller/HomeController.java
+++ SpringMVC/src/main/java/com/devanshu/springMVC/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.devanshu.springMVC.controller;
 
 import com.devanshu.springMVC.model.Alien;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
 
     @GetMapping("/")
